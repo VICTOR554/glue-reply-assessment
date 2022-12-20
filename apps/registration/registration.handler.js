411d00694f65df02be212ee3f6a29eeb73bac5d9
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2022-12-20 20:25:25 +0000

    Updated createUser function to take req.body and save it into user.json.

--- apps/registration/registration.handler.js
+++ apps/registration/registration.handler.js
@@ -7,7 +7,27 @@ const getAllUser = function (req, res) {
 };
 
 const createUser = function (req, res) {
-  res.status(200).json({ success: true });
+  let result = {
+    status: '',
+    msg: '',
+    data: user,
+  };
+
+  user.push({
+    name: req.body.name,
+    email: req.body.email,
+    dateOfBirth: req.body.dateOfBirth,
+    creditCard: req.body.creditCard,
+    password: req.body.password,
+  });
+
+  const data = JSON.stringify(user, null, 2);
+  fs.writeFile('./data/user.json', data, (err) => {
+    result.status = '201';
+    result.msg = 'success';
+
+    res.status(result.status).send(result);
+  });
 };
 
 module.exports = {
