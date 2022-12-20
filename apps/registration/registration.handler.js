99f9d677767de446dc30dee3386ff77a948c21a7
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2022-12-20 19:46:26 +0000

    parse raw data from reading user.json to json object

--- apps/registration/registration.handler.js
+++ apps/registration/registration.handler.js
@@ -1,5 +1,6 @@
 const fs = require('fs');
 const data = fs.readFileSync('./data/user.json');
+const user = JSON.parse(data);
 
 const getAllUser = function (req, res) {
   res.status(200).json({ success: true });
