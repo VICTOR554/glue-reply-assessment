4b488ab1bb8d0ece66ef577a06279a113976be53
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2022-12-21 11:29:36 +0000

    Update payment.handler.js

--- apps/payment/payment.handler.js
+++ apps/payment/payment.handler.js
@@ -2,6 +2,8 @@ const fs = require('fs');
 const database = fs.readFileSync('./data/payment.json');
 const payment = JSON.parse(database);
 const functions = require('../../utility/function');
+const database2 = fs.readFileSync('./data/user.json');
+const user = JSON.parse(database2);
 
 const getAllPayment = function (req, res) {
   res.status(200).json({ success: true, count: payment.length, data: payment });
@@ -9,8 +11,8 @@ const getAllPayment = function (req, res) {
 
 const createPayment = function (req, res) {
   let result = {
-    status: 201,
-    msg: 'success',
+    status: '',
+    msg: '',
     count: payment.length,
     data: payment,
   };
@@ -18,7 +20,7 @@ const createPayment = function (req, res) {
   let data = req.body;
 
   //function to validate Payment Parameter
-  result = functions.validateRegistrationParameter(data, result);
+  result = functions.validatePaymentParameter(data, result);
 
   if (!result.status) {
     payment.push({
@@ -29,16 +31,18 @@ const createPayment = function (req, res) {
     const database = JSON.stringify(payment, null, 2);
     fs.writeFile('./data/payment.json', database, (err) => {
       if (err) {
-        result.status = '400';
+        result.status = 400;
         result.msg = err;
 
         res.status(result.status).send(result);
-      } else {
-        res.status(result.status).send(result);
       }
+      result.status = 201;
+      result.msg = 'Success';
+      res.status(result.status).send(result);
     });
+  } else {
+    res.status(result.status).send(result);
   }
-  res.status(result.status).send(result);
 };
 
 module.exports = {
