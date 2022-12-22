232e4afe2b3610f4af377a378396f35f7926707a
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2022-12-22 16:06:27 +0000

    Initialised newUser object and http mocks before each function

--- test/registration.test.js
+++ test/registration.test.js
@@ -3,3 +3,18 @@ const should = chai.should();
 
 const handler = require('../apps/registration/registration.handler');
 const httpMocks = require('node-mocks-http');
+
+let req, res, newUser;
+beforeEach(() => {
+  newUser = {
+    name: 'John2',
+    email: 'victor@gmail.com',
+    dateOfBirth: '12/12/2000',
+    creditCard: 1234567890123456,
+    password: 'Passcode1',
+  };
+
+  // httpMocks
+  req = httpMocks.createRequest();
+  res = httpMocks.createResponse();
+});
