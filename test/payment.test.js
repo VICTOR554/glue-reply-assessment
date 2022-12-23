7b113c526d96bd4c312cbc0cc34c6ce8a92211c0
VICTOR554 <32734670+VICTOR554@users.noreply.github.com>
2022-12-23 19:34:20 +0000

    Updated payment unit test to check route and response json

--- test/payment.test.js
+++ test/payment.test.js
@@ -1,37 +1,48 @@
 const chai = require('chai');
 const should = chai.should();
-
+const fs = require('fs');
+const database = fs.readFileSync('./data/payment.json');
+const payment = JSON.parse(database);
 const handler = require('../apps/payment/payment.handler');
 const httpMocks = require('node-mocks-http');
 
 let req, res, newPayment;
+
 beforeEach(() => {
   newPayment = {
     creditCard: 1234567890123456,
     amount: 123,
   };
-
-  //  httpMocks
-  req = httpMocks.createRequest();
-  res = httpMocks.createResponse();
 });
 
 describe('Get All Payment function', () => {
   context('Get All Payment to be tested', () => {
     after(() => {
       console.log('=========after');
-      // console.log('res   :' + Object.keys(req));
-      // console.log('res2  2  :' + res.statusCode);
-      // console.log('res2  2  :' + res.statusMessage);
-      // console.log('req   :' + req.params);
+      console.log('resStatus   :' + res.statusCode);
+      console.log('=============================');
     });
+
+    beforeEach(() => {
+      // httpMocks
+      req = httpMocks.createRequest({
+        method: 'GET',
+        url: '/payment',
+      });
+      res = httpMocks.createResponse();
+    });
+
     it('it should get all payment from the database', () => {
       handler.getAllPayment(req, res);
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(201);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('All the payment in the database');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('All the payment in the database');
+      resData.data.should.be.an('array');
+      resData.data.should.be.eql(payment);
     });
   });
 });
@@ -39,12 +50,21 @@ describe('Get All Payment function', () => {
 describe('Create Payment Function', () => {
   afterEach(() => {
     console.log('=========afterEach');
-    console.log('res2    :' + res.statusCode);
+    console.log('resStatus    :' + res.statusCode);
     console.log('=============================');
   });
+
   beforeEach(() => {
+    // httpMocks
+    req = httpMocks.createRequest({
+      method: 'POST',
+      url: '/payment/new',
+    });
+    res = httpMocks.createResponse();
+
     req.body = newPayment;
   });
+
   context('Credit Card to be tested', () => {
     it('it should check Credit Card is not empty', () => {
       req.body.creditCard = '';
@@ -52,37 +72,46 @@ describe('Create Payment Function', () => {
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('Credit card parameter is not valid');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Credit card parameter is not valid');
     });
+
     it('it should check Credit Card is wrong data type', () => {
       req.body.creditCard = 'credit';
       handler.createPayment(req, res);
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('Credit card parameter is not valid');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Credit card parameter is not valid');
     });
+
     it('it should check Credit Card is wrong length', () => {
       req.body.creditCard = 1234;
       handler.createPayment(req, res);
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain(
-        'Credit Card is not registeredCredit card parameter should have 16 digits'
-      );
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Credit card parameter should have 16 digits');
     });
+
     it('it should check Credit Card is registered to a user', () => {
-      req.body.creditCard = 1234567890123450;
+      req.body.creditCard = 1212121212121212;
       handler.createPayment(req, res);
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(404);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('Credit Card is not registered');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Credit Card is not registered');
     });
   });
 
@@ -93,8 +122,10 @@ describe('Create Payment Function', () => {
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('Amount parameter is not valid');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Amount parameter is not valid');
     });
     it('it should check amount is wrong data type', () => {
       req.body.amount = '2';
@@ -102,29 +133,31 @@ describe('Create Payment Function', () => {
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain('Amount parameter is not valid');
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Amount parameter is not valid');
     });
-    it('it should check amount is wrong format ', () => {
+    it('it should check amount is less than 3 digits ', () => {
       req.body.amount = 2033;
       handler.createPayment(req, res);
       res.should.be.a('object');
       res.should.have.property('statusCode');
       res.statusCode.should.equal(400);
-      res.should.have.property('statusMessage');
-      res.statusMessage.should.contain(
-        'Amount should have a maximum of 3 digits'
-      );
+      resData = res._getJSONData();
+      resData.should.be.a('object');
+      resData.should.have.property('msg');
+      resData.msg.should.equal('Amount should have a maximum of 3 digits');
     });
   });
 
   context('Payment is valid to be tested', () => {
-    // after(() => {
-    //   console.log('=========after');
-    //   // console.log('res   :' + Object.keys(res));
-    //   console.log('res2  2  :' + res.statusCode);
-    //   // handler.createUser(req, res).removeAllListeners('event');
-    // });
+    after(() => {
+      console.log('=========after');
+      // console.log('res   :' + Object.keys(res));
+      console.log('res2  2  :' + res.statusCode);
+      // handler.createUser(req, res).removeAllListeners('event');
+    });
     it('it payment is valid', () => {
       handler.createPayment(req, res);
       res.should.be.a('object');
@@ -133,5 +166,15 @@ describe('Create Payment Function', () => {
       res.should.have.property('statusMessage');
       res.statusMessage.should.contain('Success');
     });
+    // it('it payment is valid', () => {
+    //   handler.createPayment(req, res);
+    //   res.should.be.a('object');
+    //   res.should.have.property('statusCode');
+    //   res.statusCode.should.equal(201);
+    //   resData = res._getJSONData();
+    //   resData.should.be.a('object');
+    //   resData.should.have.property('msg');
+    //   resData.msg.should.equal('Success');
+    // });
   });
 });
